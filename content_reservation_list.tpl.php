84dd3ef10717c9545cfd7ce404531a2ecdaf9c66
Ameer Ayoub <ameer.ayoub@gmail.com>
2011-04-06 17:29:26 -0700

    Got everything to work, implemented basic content list view

---
+++ content_reservation_list.tpl.php
@@ -0,0 +1,11 @@
+<?php if($content_list): ?>
+<div id="content_reservation_list-wrapper" class="content_reservation">
+<ul id="content_reservation_list" class="content_reservation">
+<?php foreach($content_list as $reservable_content): ?>
+	<li><a href="<?php echo $reservable_content->reservation_url; ?>"><?php echo $reservable_content->title; ?></a></li>
+<?php endforeach; ?>
+</ul>
+</div>
+<?php else: ?>
+<h2>Content List Empty</h2>
+<?php endif; ?>
