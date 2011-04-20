8b3974e2b4b51f9ab7e1d955e22b36e046f1d62d
Ameer Ayoub <ameer.ayoub@gmail.com>
2011-04-20 14:46:54 -0700

    Changed the display format

--- content_reservation_reserve.tpl.php
+++ content_reservation_reserve.tpl.php
@@ -1,7 +1,11 @@
-<?php print $variables['variables']['content_link']; ?>
+<a href="<?php print $variables['variables']['content_link']; ?>" class="content_link">
+&lt;&lt; back to <?php print $variables['variables']['content_title'] ?> list</a>
+<table><tr><td style="width: 310px; vertical-align: top;">
 <?php print $variables['variables']['reservation_form']; ?>
+</td><td>
 <?php print $variables['variables']['calendar_div']; ?>
-
+</td></tr>
+</table>
 <?php if(isset($variables['variables']['admin_form'])): ?>
 	<?php print $variables['variables']['admin_form']; ?>
 <?php endif; ?>
