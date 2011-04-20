eb5b0eadaaf98ee7c0252cd27e6fd3ea8a684e1e
Ameer Ayoub <ameer.ayoub@gmail.com>
2011-04-20 13:27:47 -0700

    Middle of adding the content reservation template

---
+++ content_reservation_reserve.tpl.php
@@ -0,0 +1,13 @@
+<?php if($content_list): ?>
+<div id="content_reservation_list-wrapper" class="content_reservation">
+<ul id="content_reservation_list" class="content_reservation">
+<?php foreach($content_list as $reservable_content): ?>
+	<li><div class="colorBlock" style="background-color: <?php echo $reservable_content->color; ?>;"></div><a href="<?php echo $reservable_content->reservation_url; ?>"><?php echo $reservable_content->title; ?></a></li>
+<?php endforeach; ?>
+</ul>
+<h2>Current Reservations</h2>
+<div id="content_reservation-fullCalendar"></div>
+</div>
+<?php else: ?>
+<h2>Content List Empty</h2>
+<?php endif; ?>
