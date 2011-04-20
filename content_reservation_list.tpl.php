b8189d0ac0bf92b7a14ec1f03ac392783f9df7f3
Ameer Ayoub <ameer.ayoub@gmail.com>
2011-04-20 16:07:42 -0700

    Added an edit form, still need to add edit handlers, but for now changing the reservation url scheme

--- content_reservation_list.tpl.php
+++ content_reservation_list.tpl.php
@@ -1,13 +1,14 @@
 <?php if($content_list): ?>
 <div id="content_reservation_list-wrapper" class="content_reservation">
+<table><tr><td>
+<div id="content_reservation-fullCalendar"></div>
+</div>
+</td><td style="width: 200px;">
 <ul id="content_reservation_list" class="content_reservation">
 <?php foreach($content_list as $reservable_content): ?>
 	<li><div class="colorBlock" style="background-color: <?php echo $reservable_content->color; ?>;"></div><a href="<?php echo $reservable_content->reservation_url; ?>"><?php echo $reservable_content->title; ?></a></li>
 <?php endforeach; ?>
-</ul>
-<h2>Current Reservations</h2>
-<div id="content_reservation-fullCalendar"></div>
-</div>
+</ul></td></tr></table>
 <?php else: ?>
 <h2>Content List Empty</h2>
 <?php endif; ?>
