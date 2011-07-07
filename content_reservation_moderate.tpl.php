8bc99d9269022cbb86f081ebce34117bb4315a20
Ameer Ayoub <ameer.ayoub@gmail.com>
2011-07-07 16:09:26 -0700

    More styling changes

--- content_reservation_moderate.tpl.php
+++ content_reservation_moderate.tpl.php
@@ -9,11 +9,13 @@
 <?php foreach($content_list as $event): ?>
 	<li class="moderate <?php if ($counter%2 == 0){ echo "even"; } else { echo "odd"; } $counter++; ?>">
 	<img src="<?php echo $event->show_image; ?>" alt="Show on Calendar" onclick="<?php echo $event->show_js; ?>" style="cursor: pointer;"/>
-	<a href="<?php echo $event->reservation_url; ?>"><?php echo $event->description; ?></a><br />
+	<?php echo $event->description; ?>
+	| <a href="<?php echo $event->content_url; ?>" style="color: <?php echo $event->content_url_textColor; ?>; background-color: <?php echo $event->content_url_color; ?>; padding: 0px 5px;"><?php echo $event->content_name; ?></a><br />
 	<a href="<?php echo $event->user_url; ?>"><img src="<?php echo $event->user_image; ?>" /> <?php echo $event->name; ?></a><br />
 	<span class="time-range" style="font-size: 8pt;"><?php echo $event->start; ?> to <?php echo $event->end; ?></span><br />
-	<a href="<?php echo $event->approve_url; ?>"><img src="<?php echo $event->approve_image; ?>" /> approve</a>
-	<a href="<?php echo $event->reject_url; ?>"><img src="<?php echo $event->reject_image; ?>" /> reject</a>
+	<a href="<?php echo $event->reservation_url; ?>" style="margin-right: 10px;"><img src="<?php echo $event->edit_image; ?>"  style="position: relative; top: 3px;"/> edit</a>
+	<a href="<?php echo $event->approve_url; ?>" style="margin-right: 10px;"><img src="<?php echo $event->approve_image; ?>" style="position: relative; top: 3px;"/> approve</a>
+	<a href="<?php echo $event->reject_url; ?>"><img src="<?php echo $event->reject_image; ?>" style="position: relative; top: 3px;"/> reject</a>
 	</li>
 <?php endforeach; ?>
 </ul></td></tr></table>
