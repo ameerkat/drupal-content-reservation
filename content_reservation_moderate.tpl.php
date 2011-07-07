448b11585c6fdcff39af60b3f8e898ecb6ae36d4
Ameer Ayoub <ameer.ayoub@gmail.com>
2011-07-07 15:38:00 -0700

    Added user info to moderation queue page

--- content_reservation_moderate.tpl.php
+++ content_reservation_moderate.tpl.php
@@ -3,13 +3,14 @@
 <table><tr><td>
 <div id="content_reservation-fullCalendar"></div>
 </div>
-</td><td id="content_reservation_list_td">
+</td><td id="content_reservation_list_td" class="moderate">
 <ul id="content_reservation_list" class="content_reservation">
 <?php foreach($content_list as $event): ?>
-	<li>
+	<li class="moderate">
 	<img src="<?php echo $event->show_image; ?>" alt="Show on Calendar" onclick="<?php echo $event->show_js; ?>" style="cursor: pointer;"/>
 	<a href="<?php echo $event->reservation_url; ?>"><?php echo $event->description; ?></a><br />
-	<?php echo $event->start; ?> : <?php echo $event->end; ?><br />
+	<a href="<?php echo $event->user_url; ?>"><img src="<?php echo $event->user_image; ?>" /> <?php echo $event->name; ?></a><br />
+	<span class="time-range" style="font-size: 8pt;"><?php echo $event->start; ?> to<br /> <?php echo $event->end; ?></span><br />
 	<a href="<?php echo $event->approve_url; ?>"><img src="<?php echo $event->approve_image; ?>" /> approve</a>
 	<a href="<?php echo $event->reject_url; ?>"><img src="<?php echo $event->reject_image; ?>" /> reject</a>
 	</li>
